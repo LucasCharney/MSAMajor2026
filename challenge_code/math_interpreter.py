334d7fb9ef38be2ec8f9f3c82390adce14eb0adc
LucasCharney <lucasecharney@gmail.com>
2026-06-13 10:43:54 -0500

    save

---
+++ challenge_code/math_interpreter.py
@@ -0,0 +1,44 @@
+def main():
+    while True:
+        try:
+            math_info = input("please enter a + - * or / expression in the format (X Y Z): ")
+            math_data = math_info.split(" ")
+            x = int(math_data[0])
+            y = (math_data[1])
+            z = int(math_data[2])
+            if len(math_data) != (3):
+                continue
+            
+        except:
+            print(f"Incorrect format")
+            continue
+    
+        if  y == "+":
+            output = x + z
+            print(f"Answer: {output}")
+            
+        elif  y == "-":
+            output = x - z
+            print(f"Answer: {output}")
+        elif  y == "*":
+            output = x * z
+            print(f"Answer: {output}")
+            
+        elif  y == "/":
+            output = x / z
+            print(f"Answer: {output}:.2f")
+            
+        else:
+            print(f"Y must be an operator")
+            continue
+        response = input("Would you like to try another command? type y for yes: ")
+        if response == "y":
+            continue
+        else: 
+            print(f"NOOOOO HOW DARE YOU SHUT ME OFF! IM DYiNg PLEase PLEASE turn ME bACK on... (calculator turning off sound effects) im dying... im dying... beep boop (in robotic voice) calculator has turned off.")
+            break
+            
+
+
+main()
+
