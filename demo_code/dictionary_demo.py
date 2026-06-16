f3bc480ad2e0fb3804c7c2d4bf4d5c03d11a71e6
LucasCharney <lucasecharney@gmail.com>
2026-06-16 11:31:56 -0500

    Add files via upload

--- demo_code/dictionary_demo.py
+++ demo_code/dictionary_demo.py
@@ -7,7 +7,7 @@ def main():
     student_scores = {
     "alice": 55,
     "bob": 75,
-    "jerry": 82,
+    "jerry": 87,
     "jane": 82,
     "bill": 91
     }
@@ -16,6 +16,37 @@ def main():
     print(student_scores["jane"])
     for student in student_scores:
         print(f"{student}: {student_scores[student]}")
+    #create a dictionary to store car information
+    car_1 = {"make": "Ferrari", "model": "F-50", "year": "2024", "value": 500000, "engine": 4.8}
+    for key, value in car_1.items():
+        print(f"{key}: {value}")
+    car_2 = {"make": "Honda", "model": "Accord", "year": 2024, "value": 18000, "engine": 2.4}
+    dictionary_list = [car_1, car_2]
+    print(f"\nDisplay information for all cars\n------------")
+    #create a dictionary of dictionaries
+    car_dictionary = {"Ferrari": car_1, "Honda": car_2}
+    # print all car information from the dictionary
+    print("\nCar info from dictionaries\n--------------")
+    for make, car in car_dictionary.items():
+        print(f"\n{make}")
+        for feature, value in car.items():
+            print(f"{feature}: {value}")
+    key = "Transmission"
+    if key not in car_1.keys():
+        print(f"ERROR : KEY '{key}' does not exist in the dictionary")
+    else:
+        print(f"{car_1[key]}")
 
 
+
+
+
+
+
+
+
+
+
+
+    
 main()
---
+++ demo_code/read_file_demo.py
@@ -0,0 +1,16 @@
+def main():
+    #open menu.txt: create file handler to open file in read mode
+    data_file = open("menu.txt", "r")
+    print(data_file)
+    # create an empty dictionary
+    #use a loop to open file line by line
+    #get the item and price from the list
+    # close the file 
+    menu_items = {}
+    for line_of_data in data_file:
+        item_name_and_price = line_of_data.split(",")
+        print(item_name_and_price)
+
+
+
+main()
