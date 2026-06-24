1937710e0e76733e2118afab2014fc70d02218a6
LucasCharney <lucasecharney@gmail.com>
2026-06-24 11:49:10 -0500

    Add files via upload

---
+++ challenge_code/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, url_for, redirect, abort, flash
+import requests
+
+#make a Flask app
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+#set secret key
+app.config["SECRET_KEY"] = "your secret key"
+
+"""
+Function to request student data from the api
+Input: url
+Output: JSON student data
+"""
+def get_student_data(url:str):
+    #make a request
+    #convert the format to json
+    #return the response
+    response = requests.get(url)
+    response_json = response.json()
+    return response_json
+@app.route('/', methods=['GET'])
+def index():
+    url = "http://127.0.0.1:5000/api/students/all"
+    student_data = get_student_data(url)
+    return render_template('index.html', student_data=student_data)
+#run the flask app
+app.run(port=5001)
